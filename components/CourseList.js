33469eeed43a222d516283f5f3c6fe93f5bb850b
Alex Chao <alexchao2022@u.northwestern.edu>
2021-01-26 18:24:07 -0600

    course filtering

--- components/CourseList.js
+++ components/CourseList.js
@@ -1,16 +1,30 @@
 import Course from './Course';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, ScrollView, View } from 'react-native';
+import TermSelector from './TermSelector'
 
+const termMap = { F: 'Fall', W: 'Winter', S: 'Spring'};
+const terms = Object.values(termMap);
 
-const CourseList = ({courses}) => (
+const getCourseTerm = course => (
+  termMap[course.id.charAt(0)]
+)
+
+const CourseList = ({courses}) => {
+  const [selectedTerm, setSelectedTerm] = useState('Winter');
+  const termCourses = courses.filter(course => selectedTerm === getCourseTerm(course));
+
+  return (
     <ScrollView>
       <View style={styles.courseList}>
-        {courses.map(course => <Course key={course.id} course={course}/>)}
+        <TermSelector selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm}/>
+        <ScrollView contentContainerStyle={styles.courseList}>
+          {termCourses.map(course => <Course key={course.id} course={course}/>)}
+        </ScrollView>
       </View>
     </ScrollView>
     
-  );
+  )};
 
 const styles = StyleSheet.create({ 
     courseList: {
