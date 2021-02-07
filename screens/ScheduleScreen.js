151f856282bc9f66700329302bff69a13a4c18b2
Alex Chao <alexchao2022@u.northwestern.edu>
2021-02-07 13:30:07 -0600

    database fetch

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Course from '../components/Course.js';
 import CourseList from '../components/CourseList';
 import { SafeAreaView, ScrollView, TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import UserContext from '../utils/UserContext';
+import { firebase } from '../firebase.js';
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -34,23 +36,29 @@ const Banner = ({title}) => (
   <Text style={styles.bannerStyle}>{title}</Text>
 );
 
+const fixCourses = json => ({
+  ...json,
+  courses: Object.values(json.courses)
+})
 
 
 const ScheduleScreen = ({navigation}) => {
+  const user = useContext(UserContext);
+  const canEdit = user && user.role === 'admin';
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
 
   const view = (course) => {
-    navigation.navigate('CourseDetailScreen', { course });
+    navigation.navigate(canEdit ? 'CourseEditScreen' : 'CourseDetailScreen', { course });
     };
 
   useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch('https://courses.cs.northwestern.edu/394/data/cs-courses.php');
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(json);
-    };
-    fetchSchedule();
+    const db = firebase.database().ref();
+
+    const handleData = snap => {
+      if (snap.val()) setSchedule(fixCourses(snap.val()));
+    }
+    db.on('value', handleData, error => alert(error));
+    return () => { db.off('value', handleData); };
   }, []);
 
   return (
