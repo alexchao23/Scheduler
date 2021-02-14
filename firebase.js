fa814e70971d5aed517e7792e4ecf5fb8b4188cf
Alex Chao <alexchao2022@u.northwestern.edu>
2021-02-13 22:50:23 -0600

    authenticate users working in expo

--- firebase.js
+++ firebase.js
@@ -3,15 +3,15 @@ import "firebase/database";
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDMtv3czkS_xHpFIf_oyIvAEop8EWDGuZ4",
-    authDomain: "scheduler-8d074.firebaseapp.com",
-    databaseURL: "https://scheduler-8d074-default-rtdb.firebaseio.com",
-    projectId: "scheduler-8d074",
-    storageBucket: "scheduler-8d074.appspot.com",
-    messagingSenderId: "21301570514",
-    appId: "1:21301570514:web:70fd6fa6500e998559f60b",
-    measurementId: "G-1V09LVC10P"
-  };
+  apiKey: "AIzaSyDMtv3czkS_xHpFIf_oyIvAEop8EWDGuZ4",
+  authDomain: "scheduler-8d074.firebaseapp.com",
+  databaseURL: "https://scheduler-8d074-default-rtdb.firebaseio.com",
+  projectId: "scheduler-8d074",
+  storageBucket: "scheduler-8d074.appspot.com",
+  messagingSenderId: "21301570514",
+  appId: "1:21301570514:web:70fd6fa6500e998559f60b",
+  measurementId: "G-1V09LVC10P"
+};
 
 firebase.initializeApp(firebaseConfig);
 //firebase.auth().signInWithEmailAndPassword(email, password);
